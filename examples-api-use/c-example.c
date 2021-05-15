5d153092a2febf6a0c77bac5e116abf78d99b9d2
MinionCZ <stengljirka@gmail.com>
2021-05-15 11:25:31 +0200

    fml

--- examples-api-use/c-example.c
+++ examples-api-use/c-example.c
@@ -9,13 +9,17 @@
 
 #include "images/formula1.h"
 #include "images/formula2.h"
-#include "images/kuba1.h"
-#include "images/kuba2.h"
+#include "images/formula3.h"
+#include "images/formula4.h"
+#include "images/formula5.h"
 #include "images/logo.h"
 #include "led-matrix-c.h"
 enum {
     FORMULA1,
     FORMULA2,
+    FORMULA3,
+    FORMULA4,
+    FORMULA5,
 };
 int *getPicture(int picture) {
     switch (picture) {
@@ -23,6 +27,12 @@ int *getPicture(int picture) {
             return (int *)&formula1[0][0];
         case FORMULA2:
             return (int *)&formula2[0][0];
+        case FORMULA3:
+            return (int *)&formula3[0][0];
+        case FORMULA4:
+            return (int *)&formula4[0][0];
+        case FORMULA5:
+            return (int *)&formula5[0][0];
     }
     return NULL;
 }
@@ -46,26 +56,23 @@ void animatePicture(struct LedCanvas *canvas, struct RGBLedMatrix *matrix, int p
                 frame[i][j] = 0;
             }
         }
-        for(int y = 0; y < 64; y++){
-            for(int x = anima; x >= 0; x--){
-              int xDisplay = anima * 3 - x * 3;
-              frame[y][xDisplay] = payload[y * 64 * 3 + x * 3];
-              frame[y][xDisplay + 1] = payload[y * 64 * 3 + x * 3 + 1];
-              frame[y][xDisplay + 2] = payload[y * 64 * 3 + x * 3 + 2];
-              
+        for (int y = 0; y < 64; y++) {
+            for (int x = anima; x >= 0; x--) {
+                int xDisplay = anima * 3 - x * 3;
+                frame[y][xDisplay] = payload[y * 64 * 3 + x * 3];
+                frame[y][xDisplay + 1] = payload[y * 64 * 3 + x * 3 + 1];
+                frame[y][xDisplay + 2] = payload[y * 64 * 3 + x * 3 + 2];
             }
         }
-          for(int y = 0; y < 64; y++){
-            for(int x = 0; x < 64; x++){
-              fprintf(stdout, "%d %d %d ;", frame[y][x * 3], frame[y][x * 3 + 1], frame[y][x * 3 + 2]);
+        for (int y = 0; y < 64; y++) {
+            for (int x = 0; x < 64; x++) {
+                fprintf(stdout, "%d %d %d ;", frame[y][x * 3], frame[y][x * 3 + 1], frame[y][x * 3 + 2]);
                 led_canvas_set_pixel(canvas, x, y, frame[y][x * 3], frame[y][x * 3 + 1], frame[y][x * 3 + 2]);
             }
             fprintf(stdout, "\n");
         }
-        
-      canvas = led_matrix_swap_on_vsync(matrix, canvas);
-      sleep(1);
 
+        canvas = led_matrix_swap_on_vsync(matrix, canvas);
     }
 }
 
@@ -90,8 +97,8 @@ int main(int argc, char **argv) {
 
     fprintf(stderr, "Size: %dx%d. Hardware gpio mapping: %s\n", width, height, options.hardware_mapping);
 
-    for (int i = FORMULA1; i <= FORMULA2; i++) {
-        animatePicture(offscreen_canvas, matrix, i);
+    for (int i = FORMULA1; i <= FORMULA5; i++) {
+        showPicture(offscreen_canvas, matrix, i);
         sleep(5);
     }
 
